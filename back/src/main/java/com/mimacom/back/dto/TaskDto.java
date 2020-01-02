0056a3c68f9e3624c691d7023c00decacbf6f012
tomasgarciapozuelo <tomas.garcia-pozuelobarrios@soprasteria.com>
2020-01-02 12:09:54 +0100

    Added id to DTO.

--- back/src/main/java/com/mimacom/back/dto/TaskDto.java
+++ back/src/main/java/com/mimacom/back/dto/TaskDto.java
@@ -1,12 +1,21 @@
 package com.mimacom.back.dto;
 
 public class TaskDto {
+    private Long id;
 
     private Boolean finished;
 
     private String title;
 
     private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public Boolean getFinished() {
         return finished;
